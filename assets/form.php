121d8794e23aa60d149ea7a135c4a3d61c445956
Joel <joelfabok@gmail.com>
2023-07-31 21:38:24 -0700

    test

--- assets/form.php
+++ assets/form.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['form_success'] = true;
 
         // Redirect back to index.html with success message
-        header("Location: ../index.html?success=1");
+        header("Location: ../index.html");
         exit;
     } else {
         // Redirect back to index.html with an error message
